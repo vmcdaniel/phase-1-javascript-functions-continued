563afc0002e88db2e3940f42cabaa093026166a7
Victoria <vmcdaniel459@gmail.com>
2024-08-09 12:31:43 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function shinyThings(shinyObject = 'special') {
+        return `You are ${flair}${shinyObject}${flair}!`
+    }
+}
+wrapAdjective('*')('a dedicated programmer')
+wrapAdjective('ll')('a dedicated programmer')
